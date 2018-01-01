4f21f54566c81926dde33485be573da05d784c2d
Nguyen Minh Tri <zymethyang@gmail.com>
2018-01-01 10:31:56 +0700

    prepare for humidity

--- src/humidityRouter.js
+++ src/humidityRouter.js
@@ -70,6 +70,47 @@ humidityRouter.route('/')
             res.end('DELETE operation not supported on');
         });
 
+        HumidityRouter.route('/HumidityDetailTime/:time')
+            .all((req, res, next) => {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.header('Access-Control-Allow-Origin', '*');
+                next();
+            })
+            .get((req, res, next) => {
+                var time = parseInt(req.params.time);
+                var user = firebase.auth().currentUser || false;
+                if(user){
+                    Humiditys.find({ uid: user.uid }).limit(time).sort({ 'updatedAt': -1 })
+                    .then(result => {
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type', 'application/json');
+                        res.json(result);
+                    })
+                    .catch(err => {
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type', 'application/json');
+                        res.json("Error");
+                        console.log(err);
+                    });
+                }else{
+                    res.statusCode = 200;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.json("Error");
+                }
+            })
+            .post((req, res, next) => {
+                res.statusCode = 403;
+                res.end('POST operation not supported');
+            })
+            .put((req, res, next) => {
+                res.statusCode = 403;
+                res.end('PUT operation not supported');
+            })
+            .delete((req, res, next) => {
+                res.end('DELETE operation not supported');
+            });
+
         humidityRouter.route('/Humiditys7Day/:gap/:day')
             .all((req, res, next) => {
                 res.statusCode = 200;
