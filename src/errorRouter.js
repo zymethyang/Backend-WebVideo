da5f9bc805b5ffd3120eda2bd26239b3e4a89d77
Nguyen Minh Tri <zymethyang@gmail.com>
2018-02-06 23:19:24 +0700

    finished change code to value

--- src/errorRouter.js
+++ src/errorRouter.js
@@ -5,6 +5,7 @@ const errorRouter = express.Router();
 errorRouter.use(bodyParser.json());
 
 const Errors = require('./models/error');
+const Code = require('./models/code');
 const Messagings = require('./models/messagings');
 var FieldValue = require('firebase-admin').firestore.FieldValue;
 var moment = require('moment');
@@ -50,4 +51,38 @@ errorRouter.route('/')
         res.end('DELETE operation not supported on /feedback');
     });
 
+
+    errorRouter.route('/code/:code')
+        .all((req, res, next) => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.header('Access-Control-Allow-Origin', '*');
+            next();
+        })
+        .get((req, res) => {
+          var numCode = parseInt(req.params.code);
+          Code.findOne({code:numCode})
+             .then(error => {
+                 res.statusCode = 200;
+                 res.setHeader('Content-Type', 'application/json');
+                 res.json(error);
+             })
+             .catch(err => {
+                 res.statusCode = 200;
+                 res.setHeader('Content-Type', 'application/json');
+                 res.json('Error');
+             });
+        })
+        .post((req, res, next) => {
+          res.statusCode = 403;
+          res.end('POST operation not supported on /errors');
+        })
+        .put((req, res, next) => {
+            res.statusCode = 403;
+            res.end('PUT operation not supported on /error');
+        })
+        .delete((req, res, next) => {
+            res.statusCode = 403;
+            res.end('DELETE operation not supported on /error');
+        });
 module.exports = errorRouter;
